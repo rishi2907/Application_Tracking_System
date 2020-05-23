1fc17959cb00598050652e4bb66800b6ae791884
NIKS <jainnikhil1998@gmail.com>
2020-05-23 23:50:14 +0530

    js files

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
+const JobSchema = require('../models/JobSchema');
 
+// router.get('/showApplications/pending', adminController.showApplication);
+router.get('/pending/:jobID', adminController.showApplication);
+router.get('/showJobs/pending', adminController.showJob);
 router.post('/postjob', adminController.postJobForm);
 router.get('/postjob', adminController.getJobForm);
 router.get('/register', adminController.getSignup);
@@ -9,13 +13,16 @@ router.get('/showChart', adminController.showChart);
 router.post('/register', adminController.postSignup);
 router.get('/login', adminController.getLogin);
 router.post('/login', adminController.postLogin);
-router.get('/', function(req, res, next) {
+router.get('/',async function(req, res, next) {
   if(req.user){
-    console.log(req.user);
+    // var data = await JobSchema.find({status:"open"});
+    var data = await JobSchema.find();
     res.render('admin/admin',
     {
       name: req.user.name,
-      usertype: req.user.usertype });
+      usertype: req.user.usertype,
+      Jobdata: data
+     });
   }
   else{
       res.redirect('/admin/login');
