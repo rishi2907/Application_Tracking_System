1b5f299a1423973c43af2e5b7a11138e8a1a58ed
rishi2907 <dev.rishiagrawal@gmail.com>
2020-07-06 12:54:40 +0530

    dynamicpart1

--- public/admin/js/data.js
+++ public/admin/js/data.js
@@ -1,59 +1,205 @@
-let fieldName = [
+
+let jobInformation = [
     {
-        "field_name":"Name",
-        "field_type":"text",
-        "selected": true
+        "field_name": "Job Title",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Gender",
-        "field_type":"radio",
-        "field_value":["Male","Female"],
-        "selected":true
+        "field_name": "Salary Offered",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Upload_Passport_Size_Photo",
-        "field_type":"file",
-        "selected":true
+        "field_name": "Experience Required",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Correspondence_Address",
-        "field_type":"text",
-        "selected":true
+        "field_name": "Eligibility",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Permanent_Address",
-        "field_type":"text",
-        "selected":true
+        "field_name": "Apply By",
+        "field_type": "date",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Email",
-        "field_type":"text",
-        "selected":true
+        "field_name": "Type of Opening",
+        "field_type": "radio",
+        "field_value": ["Technical Opening", "Non-Technical Opening"],
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Contact_Number",
-        "field_type":"text",
-        "selected":true
+        "field_name": "Application Fee",
+        "field_type": "number",
+        "selected": true,
+        "important": true,
     },
     {
-        "field_name":"Date_Of_Birth",
-        "field_type":"text",
-        "selected":true
-    },
-    {
-        "field_name":"Nationality",
-        "field_type":"text",
-        "selected":true
-    },
-    {
-        "field_name":"Discipline",
-        "field_type":"text",
-        "selected":true
-    },
-    {
-        "field_name":"Specialisation",
-        "field_type":"text",
-        "selected":true
+        "field_name": "Description",
+        "field_type": "textarea",
+        "selected": true,
+        "important": true,
     }
- 
+];
+
+let personalInformation = [
+    {
+        "field_name": "Name",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Gender",
+        "field_type": "radio",
+        "field_value": ["Male", "Female"],
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Upload_Passport_Size_Photo",
+        "field_type": "file",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Correspondence_Address",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Permanent_Address",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Email",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Contact_Number",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Date_Of_Birth",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Nationality",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Discipline",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Specialisation",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    }
+
+];
+
+
+let educationInformation = [
+    {
+        "field_name": "High School Board",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "High School Passout Year",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "High School Percentage of Marks",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "High School Division",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+];
+
+let experienceInformation = [
+    {
+        "field_name": "Employer",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Designation",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Date Of Joining",
+        "field_type": "date",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Date Of Leaving",
+        "field_type": "date",
+        "selected": true,
+        "important": true,
+    },
+];
+
+let additionalInformation = [
+    {
+        "field_name": "Application Fee",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Designation",
+        "field_type": "text",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Date Of Joining",
+        "field_type": "date",
+        "selected": true,
+        "important": true,
+    },
+    {
+        "field_name": "Date Of Leaving",
+        "field_type": "date",
+        "selected": true,
+        "important": true,
+    },
 ];
